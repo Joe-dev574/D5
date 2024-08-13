30e9ef7ff10071f38ad37a4db020e9ae0a91253f
JDeWeese <jdeweese574@icloud.com>
2024-08-13 18:14:27 -0500

    Initial Commit

---
+++ D5/D5App.swift
@@ -0,0 +1,17 @@
+//
+//  D5App.swift
+//  D5
+//
+//  Created by Joseph DeWeese on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct D5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
